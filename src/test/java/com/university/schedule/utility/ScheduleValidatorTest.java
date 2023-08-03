c7e43287ad98a26de3790377980b6ae0892593ef
olehtsipotan <oleh.tsipotan@gmail.com>
2023-08-03 12:30:31 +0300

    fix validation test

--- src/test/java/com/university/schedule/utility/ScheduleValidatorTest.java
+++ src/test/java/com/university/schedule/utility/ScheduleValidatorTest.java
@@ -1,5 +1,6 @@
 package com.university.schedule.utility;
 
+import com.university.schedule.exception.ScheduleGenerationDateException;
 import com.university.schedule.exception.ScheduleGenerationException;
 import com.university.schedule.model.*;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -28,7 +29,7 @@ public class ScheduleValidatorTest {
 
     @ParameterizedTest
     @CsvSource(value = {"CourseName1:CourseName2:test@example.co:password:John:Doe:1:9:0:90:Lecture:GroupName:ClassroomName"}, delimiter = ':')
-    public void isValid_whenConflictByGroupAndCourse_returnFalse(String courseName1, String courseName2, String email, String password, String firstName, String lastName, Integer orderNumber, int hour, int minute, int durationMinutes, String classTypeName, String groupName, String classroomName) {
+    public void validate_whenConflictByGroupAndCourse_throwScheduleGenerationConflictException(String courseName1, String courseName2, String email, String password, String firstName, String lastName, Integer orderNumber, int hour, int minute, int durationMinutes, String classTypeName, String groupName, String classroomName) {
 
         Course course1 = new Course(courseName1);
         Course course2 = new Course(courseName2);
@@ -71,7 +72,7 @@ public class ScheduleValidatorTest {
 
     @ParameterizedTest
     @CsvSource(value = {"CourseName:test@example.co:password:John:Doe:1:9:0:90:Lecture:GroupName:ClassroomName"}, delimiter = ':')
-    public void isValid_whenNoConflicts_returnTrue(String courseName, String email, String password, String firstName, String lastName, Integer orderNumber, int hour, int minute, int durationMinutes, String classTypeName, String groupName, String classroomName) {
+    public void validate_whenNoConflicts_dontThrowAnyException(String courseName, String email, String password, String firstName, String lastName, Integer orderNumber, int hour, int minute, int durationMinutes, String classTypeName, String groupName, String classroomName) {
 
         Course course = new Course(courseName);
         Teacher teacher = new Teacher(email, password, firstName, lastName);
@@ -111,8 +112,49 @@ public class ScheduleValidatorTest {
     }
 
     @ParameterizedTest
+    @CsvSource(value = {"CourseName:test@example.co:password:John:Doe:1:9:0:90:Lecture:GroupName:ClassroomName"}, delimiter = ':')
+    public void validate_whenDateConflict_throwScheduleGenerationDateException(String courseName, String email, String password, String firstName, String lastName, Integer orderNumber, int hour, int minute, int durationMinutes, String classTypeName, String groupName, String classroomName) {
+
+        Course course = new Course(courseName);
+        Teacher teacher = new Teacher(email, password, firstName, lastName);
+        ClassTime classTime = new ClassTime(orderNumber, LocalTime.of(hour, minute), Duration.ofMinutes(durationMinutes));
+        ClassType classType = new ClassType(classTypeName);
+        Classroom classroom = new Classroom(classroomName);
+        Group group = new Group(groupName);
+
+        LocalDate startDate = LocalDate.of(2023, 1, 2);
+        LocalDate endDate = LocalDate.of(2023, 1, 8);
+
+        List<DayScheduleItem> dayScheduleItemList = new ArrayList<>();
+        DayScheduleItem dayScheduleItem1 = DayScheduleItem.builder()
+            .course(course)
+            .teacher(teacher)
+            .classTime(classTime)
+            .dayOfWeek(DayOfWeek.MONDAY)
+            .classType(classType)
+            .groups(Set.of(group))
+            .classroom(classroom)
+            .build();
+
+        DayScheduleItem dayScheduleItem2 = DayScheduleItem.builder()
+            .course(course)
+            .teacher(teacher)
+            .classTime(classTime)
+            .dayOfWeek(DayOfWeek.THURSDAY)
+            .classType(classType)
+            .groups(Set.of(group))
+            .classroom(classroom)
+            .build();
+
+        dayScheduleItemList.add(dayScheduleItem1);
+        dayScheduleItemList.add(dayScheduleItem2);
+
+        assertThrows(ScheduleGenerationDateException.class, () -> scheduleValidator.validate(LocalDate.of(2000, 6, 1), LocalDate.of(2000, 5, 1), dayScheduleItemList));
+    }
+
+    @ParameterizedTest
     @CsvSource(value = {"CourseName1:test1@example.co:test2@example.co:password:John:Doe:1:9:0:90:Lecture:GroupName:ClassroomName"}, delimiter = ':')
-    public void isValid_whenConflictByGroupAndTeacher_returnFalse(String courseName, String email1, String email2, String password, String firstName, String lastName, Integer orderNumber, int hour, int minute, int durationMinutes, String classTypeName, String groupName, String classroomName) {
+    public void validate_whenConflictByGroupAndTeacher_throwScheduleGenerationConflictException(String courseName, String email1, String email2, String password, String firstName, String lastName, Integer orderNumber, int hour, int minute, int durationMinutes, String classTypeName, String groupName, String classroomName) {
 
         Course course = new Course(courseName);
         Teacher teacher1 = new Teacher(email1, password, firstName, lastName);

0b9e381faca1e698b12f371d2e50caff401c10e4
olehtsipotan <oleh.tsipotan@gmail.com>
2023-10-21 23:13:50 +0300

    logical improvements

--- src/main/java/com/university/schedule/validation/PasswordFieldValidator.java
+++ src/main/java/com/university/schedule/validation/PasswordFieldValidator.java
@@ -4,8 +4,18 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 public class PasswordFieldValidator implements ConstraintValidator<Password, String> {
+
+    // (?=.*[A-Za-z]) - at least one letter
+    // (?=.*\\d) - at least one digit
+    // [A-Za-z\\d]{4,} - at least 4 characters
+    // ^ - start of the string
+    // $ - end of the string
+    // Valid Example: "Password123"
 	@Override
 	public boolean isValid(String password, ConstraintValidatorContext constraintValidatorContext) {
+        if (password == null) {
+            return false;
+        }
 		String regex = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{4,}$";
 		return password.matches(regex);
 	}

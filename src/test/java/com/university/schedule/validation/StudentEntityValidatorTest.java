44a6aaee1d52f9467de75760276e39d709ca6faf
olehtsipotan <oleh.tsipotan@gmail.com>
2023-10-22 00:04:32 +0300

    add EntityValidatorTest(s)

---
+++ src/test/java/com/university/schedule/validation/StudentEntityValidatorTest.java
@@ -0,0 +1,58 @@
+package com.university.schedule.validation;
+
+import com.university.schedule.exception.ValidationException;
+import com.university.schedule.model.Student;
+import jakarta.validation.OverridesAttribute;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(SpringExtension.class)
+public class StudentEntityValidatorTest {
+
+    private StudentEntityValidator validator;
+
+    @Mock
+    private UserEntityValidator userEntityValidator;
+
+    private final Validator jakartaValidator = Validation.buildDefaultValidatorFactory().getValidator();
+
+    @BeforeEach
+    public void setUp() {
+        validator = new StudentEntityValidator(userEntityValidator, jakartaValidator);
+    }
+
+    @Test
+    public void validate_whenStudentIsValid() {
+        Student student = new Student("email", "password", "name", "surname");
+        assertDoesNotThrow(() -> validator.validate(student));
+
+        verify(userEntityValidator).validate(student);
+    }
+
+    @ParameterizedTest
+    @NullSource
+    public void validate_whenStudentIsNull_throwIllegalArgumentException(Student nullStudent) {
+        assertThrows(IllegalArgumentException.class, () -> validator.validate(nullStudent));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    public void validate_whenStudentNameFieldIsEmptyOrNull_throwValidationException(String name) {
+        Student student = new Student("email", "password", name, "surname");
+        assertThrows(ValidationException.class, () -> validator.validate(student));
+
+        verify(userEntityValidator).validate(student);
+    }
+}
---
+++ src/test/java/com/university/schedule/validation/UserEntityValidatorTest.java
@@ -0,0 +1,65 @@
+package com.university.schedule.validation;
+
+import com.university.schedule.exception.ValidationException;
+import com.university.schedule.model.User;
+import com.university.schedule.repository.AuthorityRepository;
+import com.university.schedule.repository.UserRepository;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+public class UserEntityValidatorTest {
+
+    private UserEntityValidator validator;
+
+    @Mock
+    private UserRepository userRepository;
+
+    private final Validator jakartaValidator = Validation.buildDefaultValidatorFactory().getValidator();
+
+    @BeforeEach
+    public void setUp() {
+        validator = new UserEntityValidator(userRepository, jakartaValidator);
+    }
+
+    @Test
+    public void validate_whenUserIsValid() {
+        User user = new User("email", "password", "name", "surname");
+        assertDoesNotThrow(() -> validator.validate(user));
+    }
+
+    @ParameterizedTest
+    @NullSource
+    public void validate_whenUserIsNull_throwIllegalArgumentException(User nullUser) {
+        assertThrows(IllegalArgumentException.class, () -> validator.validate(nullUser));
+    }
+
+    @ParameterizedTest
+    @NullSource
+    public void validate_whenUserEmailFieldIsEmptyOrNull_throwValidationException(String email) {
+        User user = new User(email, "password", "name", "surname");
+        assertThrows(ValidationException.class, () -> validator.validate(user));
+    }
+
+    @Test
+    public void validate_whenUserEmailIsNotUnique_throwValidationException() {
+        String sharedEmail = "email";
+        User userToCheck = new User(sharedEmail, "password", "name", "surname");
+        User userToBeFounded = new User(sharedEmail, "password", "name", "surname");
+        when(userRepository.findByEmail(sharedEmail)).thenReturn(Optional.of(userToBeFounded));
+        assertThrows(ValidationException.class, () -> validator.validate(userToCheck));
+    }
+}

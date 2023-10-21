3f518c773f2c3a9ca9d6754ee50557e19f1d0831
olehtsipotan <oleh.tsipotan@gmail.com>
2023-10-21 23:13:05 +0300

    remove unused import in DataGenerationService

--- src/main/java/com/university/schedule/service/DataGenerationService.java
+++ src/main/java/com/university/schedule/service/DataGenerationService.java
@@ -1,7 +1,6 @@
 package com.university.schedule.service;
 
 import com.university.schedule.model.*;
-import com.university.schedule.validation.DataSchemaService;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;

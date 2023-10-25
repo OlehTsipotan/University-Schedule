9d19f6bbafc4a35529706ea3be96507c44d3b57d
olehtsipotan <oleh.tsipotan@gmail.com>
2023-10-25 22:26:58 +0300

    bad input handling and code formatting for DefaultDisciplineService

--- src/main/java/com/university/schedule/service/DefaultDisciplineService.java
+++ src/main/java/com/university/schedule/service/DefaultDisciplineService.java
@@ -22,118 +22,113 @@ import java.util.List;
 @Transactional(readOnly = true)
 public class DefaultDisciplineService implements DisciplineService {
 
-	private final DisciplineRepository disciplineRepository;
+    private final DisciplineRepository disciplineRepository;
 
-	private final DisciplineEntityValidator disciplineEntityValidator;
+    private final DisciplineEntityValidator disciplineEntityValidator;
 
-	private final ConverterService converterService;
+    private final ConverterService converterService;
 
-	@Override
-	public List<Discipline> findAll() {
-		List<Discipline> disciplines = execute(() -> disciplineRepository.findAll());
-		log.debug("Retrieved All {} Discipline", disciplines.size());
-		return disciplines;
-	}
+    @Override
+    public List<Discipline> findAll() {
+        List<Discipline> disciplines = execute(() -> disciplineRepository.findAll());
+        log.debug("Retrieved All {} Discipline", disciplines.size());
+        return disciplines;
+    }
 
-	@Override
-	public List<DisciplineDTO> findAllAsDTO() {
-		List<DisciplineDTO> disciplineDTOList =
-				execute(() -> disciplineRepository.findAll()).stream().map(this::convertToDTO).toList();
-		log.debug("Retrieved All {} Discipline", disciplineDTOList.size());
-		return disciplineDTOList;
-	}
+    @Override
+    public List<DisciplineDTO> findAllAsDTO() {
+        List<DisciplineDTO> disciplineDTOList =
+            execute(() -> disciplineRepository.findAll()).stream().map(this::convertToDTO).toList();
+        log.debug("Retrieved All {} Discipline", disciplineDTOList.size());
+        return disciplineDTOList;
+    }
 
-	@Override
-	public List<DisciplineDTO> findAllAsDTO(Pageable pageable) {
-		List<DisciplineDTO> disciplineDTOList =
-				execute(() -> disciplineRepository.findAll(pageable)).stream().map(this::convertToDTO).toList();
-		log.debug("Retrieved All {} Discipline", disciplineDTOList.size());
-		return disciplineDTOList;
-	}
+    @Override
+    public List<DisciplineDTO> findAllAsDTO(Pageable pageable) {
+        if (pageable == null) {
+            throw new IllegalArgumentException("Pageable is null");
+        }
+        List<DisciplineDTO> disciplineDTOList =
+            execute(() -> disciplineRepository.findAll(pageable)).stream().map(this::convertToDTO).toList();
+        log.debug("Retrieved All {} Discipline", disciplineDTOList.size());
+        return disciplineDTOList;
+    }
 
-	@Override
-	@Transactional
-	public Long save(Discipline discipline) {
-		execute(() -> {
-			disciplineEntityValidator.validate(discipline);
-			disciplineRepository.save(discipline);
-		});
-		log.info("saved {}", discipline);
-		return discipline.getId();
-	}
+    @Override
+    @Transactional
+    public Long save(Discipline discipline) {
+        execute(() -> {
+            disciplineEntityValidator.validate(discipline);
+            disciplineRepository.save(discipline);
+        });
+        log.info("saved {}", discipline);
+        return discipline.getId();
+    }
 
-	@Override
-	@Transactional
-	public Long save(DisciplineDTO disciplineDTO) {
-		Discipline discipline = convertToEntity(disciplineDTO);
-		execute(() -> {
-			disciplineEntityValidator.validate(discipline);
-			disciplineRepository.save(discipline);
-		});
-		log.info("saved {}", discipline);
-		return discipline.getId();
-	}
+    @Override
+    @Transactional
+    public Long save(DisciplineDTO disciplineDTO) {
+        Discipline discipline = convertToEntity(disciplineDTO);
+        execute(() -> {
+            disciplineEntityValidator.validate(discipline);
+            disciplineRepository.save(discipline);
+        });
+        log.info("saved {}", discipline);
+        return discipline.getId();
+    }
 
-	@Override
-	public DisciplineDTO findByIdAsDTO(Long id) {
-		Discipline discipline = execute(() -> disciplineRepository.findById(id)).orElseThrow(
-				() -> new ServiceException("Discipline not found"));
-		log.debug("Retrieved {}", discipline);
-		return convertToDTO(discipline);
-	}
+    @Override
+    public DisciplineDTO findByIdAsDTO(Long id) {
+        Discipline discipline = execute(() -> disciplineRepository.findById(id)).orElseThrow(
+            () -> new ServiceException("Discipline not found"));
+        log.debug("Retrieved {}", discipline);
+        return convertToDTO(discipline);
+    }
 
-	private Discipline findById(Long id) {
-		Discipline discipline = execute(() -> disciplineRepository.findById(id)).orElseThrow(
-				() -> new ServiceException("Discipline not found"));
-		log.debug("Retrieved {}", discipline);
-		return discipline;
-	}
+    @Override
+    @Transactional
+    public void deleteById(Long id) {
+        execute(() -> {
+            if (!disciplineRepository.existsById(id)) {
+                throw new DeletionFailedException("There is no ClassTime to delete with id = " + id);
+            }
+            disciplineRepository.deleteById(id);
+        });
+        log.info("Deleted id = {}", id);
+    }
 
+    private DisciplineDTO convertToDTO(Discipline discipline) {
+        return converterService.convert(discipline, DisciplineDTO.class);
+    }
 
-	@Override
-	@Transactional
-	public void deleteById(Long id) {
-		try {
-			findById(id);
-		} catch (ServiceException e) {
-			throw new DeletionFailedException("There is no Discipline to delete with id = " + id);
-		}
-		execute(() -> disciplineRepository.deleteById(id));
-		log.info("Deleted id = {}", id);
-	}
-
-	private DisciplineDTO convertToDTO(Discipline discipline) {
-		return converterService.convert(discipline, DisciplineDTO.class);
-	}
-
-	private Discipline convertToEntity(DisciplineDTO disciplineDTO) {
-		return converterService.convert(disciplineDTO, Discipline.class);
-	}
+    private Discipline convertToEntity(DisciplineDTO disciplineDTO) {
+        return converterService.convert(disciplineDTO, Discipline.class);
+    }
 
 
-	private <T> T execute(DaoSupplier<T> supplier) {
-		try {
-			return supplier.get();
-		} catch (DataAccessException e) {
-			throw new ServiceException("DAO operation failed", e);
-		}
-	}
+    private <T> T execute(DaoSupplier<T> supplier) {
+        try {
+            return supplier.get();
+        } catch (DataAccessException e) {
+            throw new ServiceException("DAO operation failed", e);
+        }
+    }
 
-	private void execute(DaoProcessor processor) {
-		try {
-			processor.process();
-		} catch (DataAccessException e) {
-			throw new ServiceException("DAO operation failed", e);
-		}
-	}
+    private void execute(DaoProcessor processor) {
+        try {
+            processor.process();
+        } catch (DataAccessException e) {
+            throw new ServiceException("DAO operation failed", e);
+        }
+    }
 
-	@FunctionalInterface
-	public interface DaoSupplier<T> {
-		T get();
-	}
+    @FunctionalInterface
+    public interface DaoSupplier<T> {
+        T get();
+    }
 
-	@FunctionalInterface
-	public interface DaoProcessor {
-		void process();
-	}
+    @FunctionalInterface
+    public interface DaoProcessor {
+        void process();
+    }
 }

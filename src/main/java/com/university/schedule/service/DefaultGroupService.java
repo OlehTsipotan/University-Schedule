35e923cf26b1f514a9ed824416cc3743127c5263
olehtsipotan <oleh.tsipotan@gmail.com>
2023-10-25 23:46:23 +0300

    GroupService bad input handling, code formatting

--- src/main/java/com/university/schedule/service/DefaultGroupService.java
+++ src/main/java/com/university/schedule/service/DefaultGroupService.java
@@ -23,127 +23,119 @@ import java.util.List;
 @Transactional(readOnly = true)
 public class DefaultGroupService implements GroupService {
 
-	private final GroupRepository groupRepository;
+    private final GroupRepository groupRepository;
 
-	private final DisciplineService disciplineService;
+    private final GroupEntityValidator groupEntityValidator;
 
-	private final CourseService courseService;
+    private final ConverterService converterService;
 
-	private final ConverterService converterService;
+    @Override
+    public List<Group> findAll() throws ServiceException {
+        List<Group> groups = execute(() -> groupRepository.findAll());
+        log.debug("Retrieved All {} Groups", groups.size());
+        return groups;
+    }
 
-	private final GroupEntityValidator groupEntityValidator;
+    @Override
+    public List<GroupDTO> findAllAsDTO() throws ServiceException {
+        List<GroupDTO> groupDTOList =
+            execute(() -> groupRepository.findAll()).stream().map(this::convertToDTO).toList();
+        log.debug("Retrieved All {} Groups", groupDTOList.size());
+        return groupDTOList;
+    }
 
-	@Override
-	public List<Group> findAll() throws ServiceException {
-		List<Group> groups = execute(() -> groupRepository.findAll());
-		log.debug("Retrieved All {} Groups", groups.size());
-		return groups;
-	}
+    @Override
+    public List<GroupDTO> findAllAsDTO(Pageable pageable) {
+        if (pageable == null) {
+            throw new IllegalArgumentException("Pageable is null");
+        }
+        List<GroupDTO> groupDTOSList =
+            execute(() -> groupRepository.findAll(pageable)).stream().map(this::convertToDTO).toList();
+        log.debug("Retrieved All {} Groups", groupDTOSList.size());
+        return groupDTOSList;
+    }
 
-	@Override
-	public List<GroupDTO> findAllAsDTO() throws ServiceException {
-		List<GroupDTO> groupDTOList =
-				execute(() -> groupRepository.findAll()).stream().map(this::convertToDTO).toList();
-		log.debug("Retrieved All {} Groups", groupDTOList.size());
-		return groupDTOList;
-	}
+    @Override
+    @Transactional
+    public Long save(Group group) {
+        execute(() -> {
+            groupEntityValidator.validate(group);
+            groupRepository.save(group);
+        });
+        log.info("saved {}", group);
+        return group.getId();
+    }
 
-	@Override
-	public List<GroupDTO> findAllAsDTO(Pageable pageable) {
-		List<GroupDTO> groupDTOSList =
-				execute(() -> groupRepository.findAll(pageable)).stream().map(this::convertToDTO).toList();
-		log.debug("Retrieved All {} Groups", groupDTOSList.size());
-		return groupDTOSList;
-	}
+    @Override
+    @Transactional
+    public Long save(GroupDTO groupDTO) {
+        Group group = convertToEntity(groupDTO);
+        execute(() -> {
+            groupEntityValidator.validate(group);
+            groupRepository.save(group);
+        });
+        log.info("saved {}", group);
+        return group.getId();
+    }
 
-	@Override
-	@Transactional
-	public Long save(Group group) {
-		execute(() -> {
-			groupEntityValidator.validate(group);
-			groupRepository.save(group);
-		});
-		log.info("saved {}", group);
-		return group.getId();
-	}
+    @Override
+    public GroupDTO findByIdAsDTO(Long id) {
+        Group group =
+            execute(() -> groupRepository.findById(id)).orElseThrow(() -> new ServiceException("Group not found"));
+        log.debug("Retrieved {}", group);
+        return convertToDTO(group);
+    }
 
-	@Override
-	@Transactional
-	public Long save(GroupDTO groupDTO) {
-		Group group = convertToEntity(groupDTO);
-		execute(() -> {
-			groupEntityValidator.validate(group);
-			groupRepository.save(group);
-		});
-		log.info("saved {}", group);
-		return group.getId();
-	}
+    @Override
+    @Transactional
+    public void deleteById(Long id) {
+        execute(() -> {
+            if (!groupRepository.existsById(id)) {
+                throw new DeletionFailedException("There is no Group to delete with id = " + id);
+            }
+            groupRepository.deleteById(id);
+        });
+        log.info("Deleted id = {}", id);
+    }
 
-	private Group findById(Long id) {
-		Group group =
-				execute(() -> groupRepository.findById(id)).orElseThrow(() -> new ServiceException("Group not found"));
-		log.debug("Retrieved {}", group);
-		return group;
-	}
+    @Override
+    public List<Group> findByDiscipline(Discipline discipline) {
+        List<Group> groups = execute(() -> groupRepository.findByDiscipline(discipline));
+        log.debug("Retrieved All {} Groups", groups.size());
+        return groups;
+    }
 
-	@Override
-	public GroupDTO findByIdAsDTO(Long id) {
-		Group group =
-				execute(() -> groupRepository.findById(id)).orElseThrow(() -> new ServiceException("Group not found"));
-		log.debug("Retrieved {}", group);
-		return convertToDTO(group);
-	}
+    private GroupDTO convertToDTO(Group source) {
+        return converterService.convert(source, GroupDTO.class);
+    }
 
-	@Override
-	@Transactional
-	public void deleteById(Long id) {
-		try {
-			findById(id);
-		} catch (ServiceException e) {
-			throw new DeletionFailedException("There is no Group to delete with id = " + id);
-		}
-		execute(() -> groupRepository.deleteById(id));
-		log.info("Deleted id = {}", id);
-	}
+    private Group convertToEntity(GroupDTO groupDTO) {
+        return converterService.convert(groupDTO, Group.class);
+    }
 
-	@Override
-	public List<Group> findByDiscipline(Discipline discipline) {
-		List<Group> groups = execute(() -> groupRepository.findByDiscipline(discipline));
-		log.debug("Retrieved All {} Groups", groups.size());
-		return groups;
-	}
+    private <T> T execute(DaoSupplier<T> supplier) {
+        try {
+            return supplier.get();
+        } catch (DataAccessException e) {
+            throw new ServiceException("DAO operation failed", e);
+        }
+    }
 
-	private GroupDTO convertToDTO(Group source) {
-		return converterService.convert(source, GroupDTO.class);
-	}
+    private void execute(DaoProcessor processor) {
+        try {
+            processor.process();
+        } catch (DataAccessException e) {
+            throw new ServiceException("DAO operation failed", e);
+        }
+    }
 
-	private Group convertToEntity(GroupDTO groupDTO) {
-		return converterService.convert(groupDTO, Group.class);
-	}
+    @FunctionalInterface
+    public interface DaoSupplier<T> {
+        T get();
+    }
 
-	private <T> T execute(DaoSupplier<T> supplier) {
-		try {
-			return supplier.get();
-		} catch (DataAccessException e) {
-			throw new ServiceException("DAO operation failed", e);
-		}
-	}
-
-	private void execute(DaoProcessor processor) {
-		try {
-			processor.process();
-		} catch (DataAccessException e) {
-			throw new ServiceException("DAO operation failed", e);
-		}
-	}
-
-	@FunctionalInterface
-	public interface DaoSupplier<T> {
-		T get();
-	}
-
-	@FunctionalInterface
-	public interface DaoProcessor {
-		void process();
-	}
+    @FunctionalInterface
+    public interface DaoProcessor {
+        void process();
+    }
 }
